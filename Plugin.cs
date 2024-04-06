a1aa0a58a6dd58e28bed482a2277acc136359e19
Rudolf Kolbe <rkolbe96@gmail.com>
2024-04-06 22:59:40 +0200

    initial commit

---
+++ Plugin.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using BepInEx;
+using BepInEx.Configuration;
+using BepInEx.Logging;
+using CPrompt;
+using HarmonyLib;
+using UnityEngine;
+
+
+namespace DestroyCities
+{
+    [BepInPlugin("DestroyCities", "DestroyCities", "1.0.0")]
+    [BepInProcess("Millennia.exe")]
+    public class Plugin : BaseUnityPlugin
+    {
+        public static Plugin Instance;
+
+        private void Awake()
+        {
+            Logger.LogInfo($"Plugin DestroyCities is getting intiated!");
+            var harmony = new Harmony("DestroyCities");
+            harmony.PatchAll();
+            Logger.LogInfo($"Plugin DestroyCities finished patching!");
+        }
+    }
+
+    [HarmonyPatch(typeof(AActionPanel))]
+    public class AActionPanelPatch
+    {
+        [HarmonyPrefix]
+        [HarmonyPatch("ParseTileAction")]
+        private static bool parseTileAction(string tileActionText, out string tooltipText, out string buttonText, out Sprite buttonSprite)
+        {
+            buttonText = "";
+            tooltipText = "";
+            buttonSprite = null;
+            if (tileActionText != AEntityTilePatch.actionDestroyCity)
+            {
+                return true;
+            }
+            buttonText = string.Format(AStringTable.Instance.GetString("UI-Tooltip-EffectDestroy"), "");
+            tooltipText = "Destroys the selected city.";
+            buttonSprite = (Resources.Load("UI/Icons/ActionDestroyImprovementIcon", typeof(Sprite)) as Sprite);
+            return false;
+        }
+
+        [HarmonyPrefix]
+        [HarmonyPatch("OnTileActionButtonPressed")]
+        private static bool onTileActionButtonPressed(AActionPanel __instance, string tileActionText)
+        {
+            if (tileActionText != "TLAC,actionDestroyCity,,")
+            {
+                return true;
+            }
+            GameObject target = Traverse.Create(__instance).Field<GameObject>("ActionTarget").Value;
+            AEntityTile tile = target.GetComponent<AEntityTile>();
+            AUIHelpers.DialogYorN("Do you really want to destroy the city?", delegate ()
+            {
+                tile.DestroyEntity(false, false, true, false);
+            }, null);
+            return false;
+        }
+    }
+
+    [HarmonyPatch(typeof(AEntityTile))]
+    public class AEntityTilePatch
+    {
+        // Token: 0x06000006 RID: 6 RVA: 0x00002124 File Offset: 0x00000324
+        [HarmonyPostfix]
+        [HarmonyPatch("GetTileActions", new Type[]
+        {
+            typeof(List<string>),
+            typeof(int),
+            typeof(string),
+            typeof(bool)
+        })]
+        private static void getTileActions(AEntityTile __instance, List<string> retval, int forPlayer)
+        {
+            if (__instance.IsCity(false) && __instance.PlayerNum == forPlayer)
+            {
+                retval.Add(actionDestroyCity);
+            }            
+        }
+
+        // Token: 0x04000002 RID: 2
+        public const string actionDestroyCity = "TLAC,actionDestroyCity,,";
+    }
+}
---
+++ README.md
@@ -0,0 +1,19 @@
+# DestroyCities
+
+A mod for Millennia that allows the player to self-owned cities.
+
+This mod is a modified version of the chinese original mod [RazeCity](https://tieba.baidu.com/p/8956647641?pn=1) that was mentioned in this [comment](https://www.reddit.com/r/millennia/comments/1bvj2ya/comment/kxzmyhu/?utm_source=share&utm_medium=web3x&utm_name=web3xcss&utm_term=1&utm_content=share_button) of the reddit thread [r/millennia - Snow Ball 115 turn +200 Research Point Per Turn](https://www.reddit.com/r/millennia/comments/1bvj2ya/snow_ball_115_turn_200_research_point_per_turn/?utm_source=share&utm_medium=web3x&utm_name=web3xcss&utm_term=1&utm_content=share_button).
+
+
+## Installation
+
+1. install [BePinEx](https://docs.bepinex.dev/articles/user_guide/installation/index.html) for Millenia
+2. drop ``DestroyCities.dll`` into ``Millenia/BePinEx/plugins``
+
+
+## Build
+
+1. clone/download this repo
+2. create a new folder in it called lib
+3. copy Assembly-CSharp.dll and UnityEngine.CoreModule.dll into lib
+4. build
